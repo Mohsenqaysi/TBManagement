7cd8cd3dce8e675a36b0760aca18f5813f5d939d
mohsenqaysi <mohsen.qaysi@ucdconnect.ie>
2017-02-11 22:21:05 +0000

    remaned age to dateOfBirth

--- app/src/main/java/com/example/mohsenqaysi/tbmanagement/FirebaseDataObjects/PatientsDetailsRegistrationDataObject.java
+++ app/src/main/java/com/example/mohsenqaysi/tbmanagement/FirebaseDataObjects/PatientsDetailsRegistrationDataObject.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class PatientsDetailsRegistrationDataObject {
 
     public String fullName;
-    public int age;
+    public int dateOfBirth;
     public String gender;
     public String phoneNumber;
     public int stageDiagnosis;
@@ -24,9 +24,9 @@ public class PatientsDetailsRegistrationDataObject {
 
     public Map<String, Object> UserDetails = new HashMap<>();
 
-    public PatientsDetailsRegistrationDataObject(String fullName, int age, String gender, String phoneNumber, int stageDiagnosis, String flatNumber, String address, String city, String area, String postalCode) {
+    public PatientsDetailsRegistrationDataObject(String fullName, int dateOfBirth, String gender, String phoneNumber, int stageDiagnosis, String flatNumber, String address, String city, String area, String postalCode) {
         this.fullName = fullName;
-        this.age = age;
+        this.dateOfBirth = dateOfBirth;
         this.gender = gender;
         this.phoneNumber = phoneNumber;
         this.stageDiagnosis = stageDiagnosis;
@@ -44,7 +44,7 @@ public class PatientsDetailsRegistrationDataObject {
         HashMap<String, Object> result = new HashMap<>();
 
         result.put("fullName", fullName);
-        result.put("age", age);
+        result.put("dateOfBirth", dateOfBirth);
         result.put("gender", gender);
         result.put("phoneNumber", phoneNumber);
         result.put("stageDiagnosis", stageDiagnosis);
