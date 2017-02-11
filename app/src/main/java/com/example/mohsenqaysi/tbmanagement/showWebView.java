656857bf5138df65bb9836b2b02587d5ef70e265
mohsenqaysi <mohsen.qaysi@ucdconnect.ie>
2017-02-11 00:42:51 +0000

    ScrollView for TB General Info layout fixed a bit still need more work to make it look good

--- app/src/main/java/com/example/mohsenqaysi/tbmanagement/showWebView.java
+++ app/src/main/java/com/example/mohsenqaysi/tbmanagement/showWebView.java
@@ -21,7 +21,7 @@ public class showWebView extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_show_web_view);
+        setContentView(R.layout.activity_tbinfo_list);
 //        webView = (WebView) findViewById(R.id.webView_ID);
 //        WebSettings webSettings = webView.getSettings();
 //        webSettings.setJavaScriptEnabled(true);
