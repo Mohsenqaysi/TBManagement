f2a1301d3c957fe00e0eaaf3ab8c3a20a1b0ba2b
mohsenqaysi <mohsen.qaysi@ucdconnect.ie>
2017-02-14 02:42:13 +0000

    the circle shape is  working 2
    .resize(200,200).transform(new RoundedTransformation(100,35))

--- app/src/main/java/com/example/mohsenqaysi/tbmanagement/Helper/RoundedTransformation.java
+++ app/src/main/java/com/example/mohsenqaysi/tbmanagement/Helper/RoundedTransformation.java
@@ -9,6 +9,7 @@ import android.graphics.RectF;
 import android.graphics.Shader;
 
 // enables hardware accelerated rounded corners
+// https://gist.github.com/aprock/6213395
 // original idea here : http://www.curious-creature.org/2012/12/11/android-recipe-1-image-with-rounded-corners/
 public class RoundedTransformation implements com.squareup.picasso.Transformation {
     private final int radius;
