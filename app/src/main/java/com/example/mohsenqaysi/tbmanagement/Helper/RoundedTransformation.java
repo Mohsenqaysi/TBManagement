f34e0af29fa69836f741db76e689cf58a09eb524
mohsenqaysi <mohsen.qaysi@ucdconnect.ie>
2017-03-03 12:23:57 +0000

    TermsAndConditions activity

--- app/src/main/java/com/example/mohsenqaysi/tbmanagement/Helper/RoundedTransformation.java
+++ app/src/main/java/com/example/mohsenqaysi/tbmanagement/Helper/RoundedTransformation.java
@@ -36,13 +36,6 @@ public class RoundedTransformation implements com.squareup.picasso.Transformatio
             source.recycle();
         }
 
-//        Paint paint1 = new Paint();
-//        paint1.setColor(Color.RED);
-//        paint1.setStyle(Paint.Style.STROKE);
-//        paint1.setAntiAlias(true);
-//        paint1.setStrokeWidth(2);
-//        canvas.drawCircle((source.getWidth() - margin)/2, (source.getHeight() - margin)/2, radius-2, paint1);
-
         return output;
     }
 
