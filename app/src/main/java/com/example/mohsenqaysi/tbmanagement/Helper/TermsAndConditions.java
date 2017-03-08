8644c083ee54e87c02828bcff1c0cd9d9e0c19eb
mohsenqaysi <mohsen.qaysi@ucdconnect.ie>
2017-03-08 22:46:04 +0000

    added the terms agreement to popup before adding a new patient

--- app/src/main/java/com/example/mohsenqaysi/tbmanagement/Helper/TermsAndConditions.java
+++ app/src/main/java/com/example/mohsenqaysi/tbmanagement/Helper/TermsAndConditions.java
@@ -1,5 +1,6 @@
 package com.example.mohsenqaysi.tbmanagement.Helper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -7,39 +8,53 @@ import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.CheckBox;
 
+import com.example.mohsenqaysi.tbmanagement.MainActivity;
+import com.example.mohsenqaysi.tbmanagement.PatientsDetails.PatientDetailsRegistrationForm;
 import com.example.mohsenqaysi.tbmanagement.R;
 
 public class TermsAndConditions extends AppCompatActivity implements View.OnClickListener {
     WebView webView;
     Button agree;
+    Button cancel;
     CheckBox checkBox;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terms_and_conditions);
 
-        agree = (Button) findViewById(R.id.termsAndConditions_ID);
+        agree = (Button) findViewById(R.id.termsAndConditionsAgree_ID);
         agree.setOnClickListener(this);
+
+        cancel = (Button) findViewById(R.id.termsAndConditionsCancel_ID);
+        cancel.setOnClickListener(this);
+
         checkBox = (CheckBox) findViewById(R.id.checkBox_ID);
         disableSignInButton();
         checkBox.setOnClickListener(this);
 
         webView = (WebView) findViewById(R.id.webView_ID);
-// path /Users/mohsenqaysi/Documents/TBManagement/app/src/main/java/com/example/mohsenqaysi/tbmanagement/Helper/web.html
         webView.loadUrl("file:///android_asset/web.html");
 
     }
 
     @Override
     public void onClick(View view) {
-
         if (view == checkBox){
-
             if (checkBox.isChecked()){
                 enableSignInButton();
             } else {
                 disableSignInButton();
             }
+        }
+
+        if (view == agree){
+            startActivity(new Intent(getApplicationContext(), PatientDetailsRegistrationForm.class));
+            finish();
+        }
+
+        if (view == cancel){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
         }
     }
 
